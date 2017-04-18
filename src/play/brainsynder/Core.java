9ff9eb1a3e9d88561d31daff19e53ac4d642ec3b
CatsCraft-Private <cbbdevs@gmailcom>
2017-04-18 13:37:43 -0400

    Updated Code

--- src/play/brainsynder/Core.java
+++ src/play/brainsynder/Core.java
@@ -1,4 +1,15 @@
 package play.brainsynder;
 
-public class Core {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+    private static Core instance;
+
+    public static Core getInstance() {
+        return instance;
+    }
+
+    @Override public void onEnable() {
+        instance = this;
+    }
 }
